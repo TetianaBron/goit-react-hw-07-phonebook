bd93cedd9952ec21f5b68c3cf44e84f4b9198513
Tetiana Bronitska <tanya.nest87@gmail.com>
2021-03-03 22:19:08 +0200

    Deleted persist

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-//import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import store from './redux/store';
-//import Spinner from './Components/Spinner/Spinner';
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './styles/base.scss';
 
@@ -12,11 +10,7 @@ console.log(store);
 
 ReactDOM.render(
     <Provider store={store}>
-        {/* <PersistGate
-            loading={<Spinner/>}
-            persistor={store.persistor}> */}
            <App/>  
-    {/* </PersistGate> */}
     </Provider>,
     document.querySelector('#root'),
 );
